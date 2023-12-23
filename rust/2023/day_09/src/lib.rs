a735be4a6fe3f17f953e488dcc885d7be4736ee9
Chris Couzens <ccouzens@gmail.com>
2023-12-23 11:20:40 +0000

    Solve 2023-09-2

--- rust/2023/day_09/src/lib.rs
+++ rust/2023/day_09/src/lib.rs
@@ -23,8 +23,25 @@ fn next_sequence_value(input: &[i64]) -> i64 {
     next_sequence_value(&differences) + input.last().unwrap()
 }
 
+fn prev_sequence_value(input: &[i64]) -> i64 {
+    if input.iter().all(|v| *v == 0) {
+        return 0;
+    }
+    let differences: Vec<i64> = input
+        .iter()
+        .zip(input.iter().skip(1))
+        .map(|(a, b)| a - b)
+        .collect();
+
+    prev_sequence_value(&differences) + input.first().unwrap()
+}
+
 pub fn part_one(input: &str) -> i64 {
     parse(input).iter().map(|l| next_sequence_value(l)).sum()
+}
+
+pub fn part_two(input: &str) -> i64 {
+    parse(input).iter().map(|l| prev_sequence_value(l)).sum()
 }
 
 #[cfg(test)]
@@ -40,5 +57,16 @@ mod tests {
     #[cfg(feature = "challenge")]
     fn part_one_challenge() {
         assert_eq!(part_one(include_str!("../input.txt")), 1898776583)
+    }
+
+    #[test]
+    fn part_two_example() {
+        assert_eq!(part_two(include_str!("../example.txt")), 2)
+    }
+
+    #[test]
+    #[cfg(feature = "challenge")]
+    fn part_two_challenge() {
+        assert_eq!(part_two(include_str!("../input.txt")), 1100)
     }
 }

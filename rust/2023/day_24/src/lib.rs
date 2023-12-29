292f0162aef6ae332111cec5370734b4246c2ea9
Chris Couzens <ccouzens@gmail.com>
2023-12-29 21:20:12 +0000

    Use the cross product to simplify equations
    By using the cross product we can ignore the time variables.
    
    I used this as a guide.
    
    I produce SymPy code, but unfortunately the challenge code doesn't
    produce any output. So I'll need to continue simplifying.
    
    https://www.reddit.com/r/adventofcode/comments/18pnycy/comment/kf9l3kw/

--- rust/2023/day_24/src/lib.rs
+++ rust/2023/day_24/src/lib.rs
@@ -42,36 +42,6 @@ impl Hailstone {
         }
         Some((x, y))
     }
-
-    fn position_at_time(&self, time: f64, dimension: usize) -> f64 {
-        self.loc[dimension] + time * self.velocity[dimension]
-    }
-
-    /**
-    Use least squares linear regression to find a hailstone that would
-    come closest to hitting the other hailstones at the provided times.
-
-    Each dimension (x, y, z) is worked out independently.
-    */
-    fn new_from_intersecting_paths(others: &[(&Self, f64)]) -> Self {
-        let mut loc = [0.0; 3];
-        let mut velocity = [0.0; 3];
-        let n = others.len() as f64;
-        let sum_x: f64 = others.iter().map(|(_h, t)| t).sum();
-        let sum_x2: f64 = others.iter().map(|(_h, t)| t * t).sum();
-        for (d, (l, v)) in loc.iter_mut().zip(velocity.iter_mut()).enumerate() {
-            let sum_x_y: f64 = others
-                .iter()
-                .map(|(h, t)| h.position_at_time(*t, d) * t)
-                .sum();
-            let sum_y: f64 = others.iter().map(|(h, t)| h.position_at_time(*t, d)).sum();
-            let m = (n * sum_x_y - sum_x * sum_y) / (n * sum_x2 - sum_x * sum_x);
-            let b = (sum_y - m * sum_x) / n;
-            *v = m;
-            *l = b;
-        }
-        Self::new_from_loc_and_velocity(loc, velocity)
-    }
 }
 
 fn parse(input: &str) -> Vec<Hailstone> {
@@ -99,74 +69,15 @@ pub fn part_one(input: &str, boundary: Range<f64>) -> u64 {
     count
 }
 
-fn score_part_two_solution(hailstones: &[(&Hailstone, f64)]) -> f64 {
-    let ideal = Hailstone::new_from_intersecting_paths(hailstones);
-    hailstones
-        .iter()
-        .map(|(other, t)| {
-            (0..3)
-                .map(|d| {
-                    let diff = other.position_at_time(*t, d) - ideal.position_at_time(*t, d);
-                    diff.abs()
-                })
-                .sum::<f64>()
-        })
-        .sum::<f64>()
-}
-
-fn iterate_to_part_two_solution(hailstones: &[Hailstone]) -> f64 {
-    let mut working_set = hailstones.iter().zip(0..).map(|(h, s)| (h, s as f64)).collect::<Vec<_>>();
-    let mut index = 0;
-    let mut score = score_part_two_solution(&working_set);
-
-    'outer: loop {
-        for direction in [-1.0, 1.0] {
-            for magnitude in (0..).map(|m| f64::powi(2.0, m)/16.0) {
-                println!("{index} {direction} {magnitude}");
-                println!("{:?} {score}", working_set.iter().map(|w| w.1).collect::<Vec<_>>());
-    let ideal = Hailstone::new_from_intersecting_paths(&working_set);
-                println!("{ideal:?}");
-                let mut candidate_set = working_set.clone();
-                candidate_set[index].1 += direction * magnitude;
-                if candidate_set[index].1 < 0.0 {
-                    println!("negative breaking");
-                    break;
-                }
-                let new_score = score_part_two_solution(&candidate_set);
-                if new_score.is_nan() {
-                    println!("nan continue");
-                    continue;
-                }
-                if new_score > score && magnitude > 1024.0 {
-                    break;
-                }
-                if new_score > score {
-                    continue;
-                }
-                working_set = candidate_set;
-                score = new_score;
-                if score == 0.0 {
-                    break 'outer;
-                }
-            }
-        }
-        index = (index + 1) % hailstones.len();
-    }
-    let ideal = Hailstone::new_from_intersecting_paths(&working_set);
-    ideal.loc.iter().sum()
-}
-
 pub fn part_two(input: &str) -> String {
     let mut output = String::new();
     let hailstones = parse(input);
     let time_variables = ['t', 'u', 'w'];
-    let location_variables = ['x', 'y', 'z'];
-    let velocity_variables = ['a', 'b', 'c'];
 
     for (hailstone, time_variable) in hailstones.iter().zip(time_variables.iter()) {
         writeln!(
             &mut output,
-            "({} {} {}) + {time_variable}({} {} {})",
+            "(x y z) + {time_variable}(a b c) - ({} {} {}) - {time_variable}({} {} {}) = (0 0 0)",
             hailstone.loc[0],
             hailstone.loc[1],
             hailstone.loc[2],
@@ -177,57 +88,100 @@ pub fn part_two(input: &str) -> String {
         .unwrap();
     }
     writeln!(&mut output).unwrap();
+
     for (hailstone, time_variable) in hailstones.iter().zip(time_variables.iter()) {
-        for (loc, (velocity, (loc_var, velocity_var))) in hailstone.loc.iter().zip(
-            hailstone
-                .velocity
-                .iter()
-                .zip(location_variables.iter().zip(velocity_variables.iter())),
-        ) {
-            writeln!(
-                &mut output,
-                "{loc_var} + {time_variable}*{velocity_var} = {loc}{velocity:+}{time_variable},"
-            )
-            .unwrap();
-        }
+        writeln!(
+            &mut output,
+            "(x y z) -  ({} {} {}) = {time_variable}({} {} {}) - {time_variable}(a b c)",
+            hailstone.loc[0],
+            hailstone.loc[1],
+            hailstone.loc[2],
+            hailstone.velocity[0],
+            hailstone.velocity[1],
+            hailstone.velocity[2]
+        )
+        .unwrap();
     }
     writeln!(&mut output).unwrap();
 
-    for (loc_i, _loc_var) in location_variables.iter().enumerate() {
+    for (hailstone, time_variable) in hailstones.iter().zip(time_variables.iter()) {
         writeln!(
             &mut output,
-            "{}{:+}{}{:+}{}-{}*{}+{}*{}=0,",
-            hailstones[0].loc[loc_i] - hailstones[1].loc[loc_i],
-            hailstones[0].velocity[loc_i],
-            time_variables[0],
-            -hailstones[1].velocity[loc_i],
-            time_variables[1],
-            time_variables[0],
-            velocity_variables[loc_i],
-            time_variables[1],
-            velocity_variables[loc_i],
-        )
-        .unwrap();
-        writeln!(
-            &mut output,
-            "{}{:+}{}{:+}{}-{}*{}+{}*{}=0,",
-            hailstones[0].loc[loc_i] - hailstones[2].loc[loc_i],
-            hailstones[0].velocity[loc_i],
-            time_variables[0],
-            -hailstones[2].velocity[loc_i],
-            time_variables[2],
-            time_variables[0],
-            velocity_variables[loc_i],
-            time_variables[2],
-            velocity_variables[loc_i],
+            "(x - {loc_x}, y - {loc_y}, z - {loc_z}) = {time_variable}({vel_x} - a, {vel_y} - b, {vel_z} - c)",
+            loc_x= hailstone.loc[0],
+            loc_y= hailstone.loc[1],
+            loc_z = hailstone.loc[2],
+            vel_x = hailstone.velocity[0],
+            vel_y = hailstone.velocity[1],
+            vel_z = hailstone.velocity[2]
         )
         .unwrap();
     }
 
     writeln!(&mut output).unwrap();
 
-    println!("{:?}", iterate_to_part_two_solution(&hailstones));
+    for hailstone in hailstones.iter().take(3) {
+        let loc_x = hailstone.loc[0];
+        let loc_y = hailstone.loc[1];
+        let loc_z = hailstone.loc[2];
+        let vel_x = hailstone.velocity[0];
+        let vel_y = hailstone.velocity[1];
+        let vel_z = hailstone.velocity[2];
+        writeln!(
+            &mut output,
+            "(y - {loc_y})*({vel_z} - c) - (z - {loc_z})*({vel_y} -b) = 0",
+        )
+        .unwrap();
+        writeln!(
+            &mut output,
+            "(z - {loc_z})*({vel_x} - a) - (x - {loc_x})*({vel_z} -c) = 0",
+        )
+        .unwrap();
+        writeln!(
+            &mut output,
+            "(x - {loc_x})*({vel_y} - b) - (y - {loc_y})*({vel_x} -a) = 0",
+        )
+        .unwrap();
+    }
 
+    writeln!(&mut output).unwrap();
+
+    writeln!(&mut output, "from sympy import solve").unwrap();
+    writeln!(&mut output, "from sympy.abc import x, y, z, a, b, c").unwrap();
+    writeln!(&mut output, "solve([").unwrap();
+    for hailstone in hailstones.iter().take(3) {
+        let loc_x = hailstone.loc[0];
+        let loc_y = hailstone.loc[1];
+        let loc_z = hailstone.loc[2];
+        let vel_x = hailstone.velocity[0];
+        let vel_y = hailstone.velocity[1];
+        let vel_z = hailstone.velocity[2];
+        writeln!(
+            &mut output,
+            "{vel_z}*y - c*y {:+} {loc_y:+}*c {:+}*z +b*z {:+}*b,",
+            -loc_y * vel_z + loc_z * vel_y,
+            -vel_y,
+            -loc_z
+        )
+        .unwrap();
+        writeln!(
+            &mut output,
+            "{vel_x}*z - a*z {:+} {loc_z:+}*a {:+}*x +c*x {:+}*c,",
+            -loc_z * vel_x + loc_x * vel_z,
+            -vel_z,
+            -loc_x
+        )
+        .unwrap();
+        writeln!(
+            &mut output,
+            "{vel_y}*x - b*x {:+} {loc_x:+}*b {:+}*y +a*y {:+}*a,",
+            -loc_x * vel_y + loc_y * vel_x,
+            -vel_x,
+            -loc_y
+        )
+        .unwrap();
+    }
+    writeln!(&mut output, "], [x, y, z, a, b, c], set=True)").unwrap();
     output
 }
 

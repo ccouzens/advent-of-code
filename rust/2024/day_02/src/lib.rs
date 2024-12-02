1f5c360fed8c7c1775f59297c2fde8c934a2743d
Chris Couzens <ccouzens@gmail.com>
2024-12-02 09:05:03 +0000

    Solve 2024 day 02 part 2
    Not super clean, but it works in a reasonable time
    so it is fine.

--- rust/2024/day_02/src/lib.rs
+++ rust/2024/day_02/src/lib.rs
@@ -17,18 +17,29 @@ fn parse(input: &str) -> Reports {
     raw.unwrap().1
 }
 
+fn safe_report(r: &[Num]) -> bool {
+    (r.iter().zip(r.iter().skip(1)).all(|(a, b)| a > b)
+        || r.iter().zip(r.iter().skip(1)).all(|(a, b)| a < b))
+        && r.iter()
+            .zip(r.iter().skip(1))
+            .all(|(&a, &b)| Num::abs_diff(a, b) <= 3)
+}
+
 pub fn part_1(input: &str) -> usize {
+    let reports = parse(input);
+    reports.iter().filter(|r| safe_report(r)).count()
+}
+
+pub fn part_2(input: &str) -> usize {
     let reports = parse(input);
     reports
         .iter()
         .filter(|r| {
-            r.iter().zip(r.iter().skip(1)).all(|(a, b)| a > b)
-                || r.iter().zip(r.iter().skip(1)).all(|(a, b)| a < b)
-        })
-        .filter(|r| {
-            r.iter()
-                .zip(r.iter().skip(1))
-                .all(|(&a, &b)| Num::abs_diff(a, b) <= 3)
+            (0..r.len()).any(move |i| {
+                let mut r_prime = (*r).clone();
+                r_prime.remove(i);
+                safe_report(&r_prime)
+            })
         })
         .count()
 }
@@ -45,5 +56,15 @@ mod tests {
     #[test]
     fn challenge_part_1() {
         assert_eq!(part_1(include_str!("../input.txt")), 326);
+    }
+
+    #[test]
+    fn example_part_2() {
+        assert_eq!(part_2(include_str!("../example_1.txt")), 4);
+    }
+
+    #[test]
+    fn challenge_part_2() {
+        assert_eq!(part_2(include_str!("../input.txt")), 381);
     }
 }

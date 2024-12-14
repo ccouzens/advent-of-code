0ed33f38d212d8abf0c6461beaa249d337083686
Chris Couzens <ccouzens@gmail.com>
2024-12-14 19:40:50 +0000

    Solve 2024 day 11 part 2

--- rust/2024/day_11/src/lib.rs
+++ rust/2024/day_11/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 type Num = u64;
 struct Pebbles(Vec<Num>);
 
@@ -11,36 +13,55 @@ impl Pebbles {
         )
     }
 
-    fn blink(&mut self) {
-        let mut following = Vec::with_capacity(self.0.len());
+    fn sequence_length_calculator(&self, iterations: usize) -> usize {
+        let mut memo = BTreeMap::<(Num, usize), usize>::new();
+        let mut count = 0;
 
         for &stone in self.0.iter() {
-            let digits = Num::checked_ilog10(stone).map(|i| i + 1);
-            match digits {
-                None => following.push(1),
-                Some(digits) if digits % 2 == 0 => {
-                    let divider = Num::pow(10, digits / 2);
-                    following.push(stone / divider);
-                    following.push(stone % divider);
-                }
-                _ => following.push(stone * 2024),
-            }
+            count += sequence_length_calculator_internal(stone, iterations, &mut memo);
         }
 
-        self.0 = following;
-    }
-
-    fn count(&self) -> usize {
-        self.0.len()
+        count
     }
 }
 
-pub fn part_1(input: &str) -> usize {
-    let mut pebbles = Pebbles::parse(input);
-    for _ in 0..25 {
-        pebbles.blink();
+fn sequence_length_calculator_internal(
+    num: Num,
+    iterations: usize,
+    memo: &mut BTreeMap<(Num, usize), usize>,
+) -> usize {
+    if iterations == 0 {
+        return 1;
     }
-    pebbles.count()
+
+    if let Some(&count) = memo.get(&(num, iterations)) {
+        return count;
+    }
+
+    let digits = Num::checked_ilog10(num).map(|i| i + 1);
+    let count = match digits {
+        None => sequence_length_calculator_internal(1, iterations - 1, memo),
+        Some(digits) if digits % 2 == 0 => {
+            let divider = Num::pow(10, digits / 2);
+
+            sequence_length_calculator_internal(num / divider, iterations - 1, memo)
+                + sequence_length_calculator_internal(num % divider, iterations - 1, memo)
+        }
+        _ => sequence_length_calculator_internal(num * 2024, iterations - 1, memo),
+    };
+
+    memo.insert((num, iterations), count);
+    count
+}
+
+pub fn part_1(input: &str) -> usize {
+    let pebbles = Pebbles::parse(input);
+    pebbles.sequence_length_calculator(25)
+}
+
+pub fn part_2(input: &str) -> usize {
+    let pebbles = Pebbles::parse(input);
+    pebbles.sequence_length_calculator(75)
 }
 
 #[cfg(test)]
@@ -55,5 +76,10 @@ mod tests {
     #[test]
     fn challenge_part_1() {
         assert_eq!(part_1(include_str!("../input.txt")), 183620);
+    }
+
+    #[test]
+    fn challenge_part_2() {
+        assert_eq!(part_2(include_str!("../input.txt")), 220377651399268);
     }
 }

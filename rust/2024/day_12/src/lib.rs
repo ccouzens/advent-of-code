e5d199f873f9d08486103bdbc678e6c30280913c
Chris Couzens <ccouzens@gmail.com>
2024-12-14 21:03:28 +0000

    Solve 2024 day 12 part 2

--- rust/2024/day_12/src/lib.rs
+++ rust/2024/day_12/src/lib.rs
@@ -53,26 +53,59 @@ impl Plot {
     }
 
     fn perimeter(&self) -> usize {
-        self.0
+        DIRECTIONS
             .iter()
-            .map(|&c| {
-                DIRECTIONS
+            .map(|&d| {
+                self.0
                     .iter()
-                    .filter(|&&d| !self.0.contains(&(c.0 + d.0, c.1 + d.1)))
+                    .filter(|&&c| !self.0.contains(&(c.0 + d.0, c.1 + d.1)))
                     .count()
             })
             .sum()
     }
 
-    fn price(&self) -> usize {
-        self.area() * self.perimeter()
+    fn number_of_sides(&self) -> usize {
+        let mut count = 0;
+        for &direction in DIRECTIONS.iter() {
+            let mut considered = BTreeSet::new();
+            let turn_a = (-direction.1, direction.0);
+            let turn_b = (direction.1, -direction.0);
+            for &c in self.0.iter() {
+                if considered.insert(c) && !self.0.contains(&(c.0 + direction.0, c.1 + direction.1))
+                {
+                    for &turn in &[turn_a, turn_b] {
+                        for i in 0.. {
+                            let neighbour = (c.0 + turn.0 * i, c.1 + turn.1 * i);
+                            if !self.0.contains(&neighbour) {
+                                break;
+                            }
+                            if self
+                                .0
+                                .contains(&(neighbour.0 + direction.0, neighbour.1 + direction.1))
+                            {
+                                break;
+                            }
+                            considered.insert(neighbour);
+                        }
+                    }
+                    count += 1;
+                }
+            }
+        }
+        count
     }
 }
 
 pub fn part_1(input: &str) -> usize {
     let garden = GardenMap::parse(input);
     let plots = garden.plots();
-    plots.iter().map(|p| p.price()).sum()
+    plots.iter().map(|p| p.area() * p.perimeter()).sum()
+}
+
+pub fn part_2(input: &str) -> usize {
+    let garden = GardenMap::parse(input);
+    let plots = garden.plots();
+    plots.iter().map(|p| p.area() * p.number_of_sides()).sum()
 }
 
 #[cfg(test)]
@@ -89,5 +122,19 @@ mod tests {
     #[test]
     fn challenge_part_1() {
         assert_eq!(part_1(include_str!("../input.txt")), 1485656);
+    }
+
+    #[test]
+    fn example_part_2() {
+        assert_eq!(part_2(include_str!("../example_1.txt")), 80);
+        assert_eq!(part_2(include_str!("../example_2.txt")), 436);
+        assert_eq!(part_2(include_str!("../example_4.txt")), 236);
+        assert_eq!(part_2(include_str!("../example_5.txt")), 368);
+        assert_eq!(part_2(include_str!("../example_3.txt")), 1206);
+    }
+
+    #[test]
+    fn challenge_part_2() {
+        assert_eq!(part_2(include_str!("../input.txt")), 899196);
     }
 }

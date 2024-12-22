148531671a9892a0b64e9adcc6396e10d49cc054
Chris Couzens <ccouzens@gmail.com>
2024-12-22 13:39:28 +0000

    Solve 2024 day 22 part 2

--- rust/2024/day_22/src/lib.rs
+++ rust/2024/day_22/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{collections::BTreeMap, iter::once};
+
 type Num = i64;
 
 fn parse(input: &str) -> impl Iterator<Item = Num> + '_ {
@@ -10,15 +12,43 @@ fn evolve(secret: Num) -> Num {
     ((secret * 2048) ^ secret) % 16777216
 }
 
+fn secret_evolution(mut secret: Num) -> impl Iterator<Item = Num> {
+    once(secret).chain((0..).map(move |_| {
+        secret = evolve(secret);
+        secret
+    }))
+}
+
+fn price_evolution(secret: Num) -> impl Iterator<Item = Num> {
+    secret_evolution(secret).map(|s| s % 10)
+}
+
 pub fn part_1(input: &str) -> Num {
     parse(input)
-        .map(|mut secret| {
-            for _ in 0..2000 {
-                secret = evolve(secret);
-            }
-            secret
-        })
+        .map(|secret| secret_evolution(secret).nth(2000).unwrap())
         .sum()
+}
+
+pub fn part_2(input: &str) -> Num {
+    let mut overall_totals: BTreeMap<[Num; 4], Num> = BTreeMap::new();
+    for hiding_spot in parse(input) {
+        let mut local_totals: BTreeMap<[Num; 4], Num> = BTreeMap::new();
+        let prices: Vec<_> = price_evolution(hiding_spot).take(2001).collect();
+        for window in prices.windows(5) {
+            local_totals
+                .entry([
+                    window[1] - window[0],
+                    window[2] - window[1],
+                    window[3] - window[2],
+                    window[4] - window[3],
+                ])
+                .or_insert(window[4]);
+        }
+        for (&seq, &bananas) in local_totals.iter() {
+            *overall_totals.entry(seq).or_default() += bananas;
+        }
+    }
+    overall_totals.values().copied().max().unwrap()
 }
 
 #[cfg(test)]
@@ -33,5 +63,15 @@ mod tests {
     #[test]
     fn challenge_part_1() {
         assert_eq!(part_1(include_str!("../input.txt")), 19847565303);
+    }
+
+    #[test]
+    fn example_part_2() {
+        assert_eq!(part_2(include_str!("../example_2.txt")), 23);
+    }
+
+    #[test]
+    fn challenge_part_2() {
+        assert_eq!(part_2(include_str!("../input.txt")), 2250);
     }
 }

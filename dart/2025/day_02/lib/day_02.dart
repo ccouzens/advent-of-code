415843e75aee687584aa45ff9566f4f4c72bf85f
Chris Couzens <ccouzens@gmail.com>
2025-12-20 17:33:01 +0000

    Swap to using iterables in the solution

--- dart/2025/day_02/lib/day_02.dart
+++ dart/2025/day_02/lib/day_02.dart
@@ -1,22 +1,26 @@
 import 'dart:math';
 
+bool part1Invalid(int productId) {
+  final digits = (log(productId) / ln10).ceil();
+  if (digits.isOdd) {
+    productId = pow(10, digits).ceil();
+    return false;
+  }
+
+  final divider = pow(10, digits / 2).ceil();
+
+  return productId ~/ divider == productId % divider;
+}
+
 int part1(String input) {
   var sum = 0;
   for (final s in input.split(',')) {
     final [start, end] = s.split('-').take(2).map(int.parse).toList();
-    for (var i = start; i <= end; i++) {
-      var digits = (log(i) / ln10).ceil();
-      if (digits.isOdd) {
-        i = pow(10, digits).ceil();
-        continue;
-      }
-
-      var divider = pow(10, digits / 2).ceil();
-
-      if (i ~/ divider == i % divider) {
-        sum += i;
-      }
-    }
+    sum += Iterable.generate(end - start + 1)
+        .map<int>((i) => i + start)
+        .where(part1Invalid)
+        .fold(0, (sum, i) => sum + i);
+    ;
   }
   return sum;
 }
